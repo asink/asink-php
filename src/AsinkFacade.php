75e923aa8f3282187ffe9f95c411360af1e4cf19
Harry Lawrence <hazbo@gmx.com>
2014-10-28 12:41:03 +0000

    Added copyright comments

--- src/AsinkFacade.php
+++ src/AsinkFacade.php
@@ -1,5 +1,24 @@
 <?php namespace Asink\Component;
 
+/**
+ * asink-php v0.0.1-dev
+ *
+ *
+ * (c) Ground Six
+ *
+ * @package asink-php
+ * @version 0.0.1-dev
+ *
+ * @author Harry Lawrence <http://github.com/hazbo>
+ *
+ * Supported for Asink v0.1.1
+ *
+ * License: MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Support\Facades\Facade;
 
 class AsinkFacade extends Facade
