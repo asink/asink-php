c1d3a8d4a92b7e9d9ae4290fc50fcb98a890475c
Harry Lawrence <hazbo@gmx.com>
2014-10-28 08:44:34 +0000

    Added comments to client

--- src/Client.php
+++ src/Client.php
@@ -1,5 +1,24 @@
 <?php namespace Asink\Component;
 
+/**
+ * asink-php v0.0.1-dev
+ *
+ *
+ * (c) Ground Six
+ *
+ * @package asink-php
+ * @version 0.0.1-dev
+ *
+ * @author Harry Lawrence <http://github.com/hazbo>
+ *
+ * Supported for Asink v0.1.1
+ *
+ * License: MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 class Client
 {
 	private
@@ -8,6 +27,11 @@ class Client
 		$guzzleClient,
 		$requestBody;
 
+	/**
+	 * Creates instance of Guzzle and defines default
+	 * host and port. Tasks is created as an empty
+	 * array
+	 */
 	public function __construct()
 	{
 		$this->baseUrl 	    = "http://localhost:3000";
@@ -15,11 +39,24 @@ class Client
 		$this->guzzleClient = new \GuzzleHttp\Client();
 	}
 
+	/**
+	 * The base URL can be overridden at this point
+	 * if it needs to be changed, port included, for
+	 * now
+	 * 
+	 * @param string $base_url The base URL
+	 */
 	public function setBaseUrl($base_url)
 	{
 		$this->baseUrl = $base_url;
 	}
 
+	/**
+	 * Adds a new task to the stack
+	 * 
+	 * @param string $name The name of the new task
+	 * @param array  $task Task and command settings
+	 */
 	public function addTask($name, $task = array())
 	{
 		if (isset($task['command'])) {
@@ -27,11 +64,17 @@ class Client
 		}
 	}
 
+	/**
+	 * Prepares the tasks for request
+	 */
 	private function prepareRequest()
 	{
 		$this->requestBody = json_encode($this->tasks);
 	}
 
+	/**
+	 * Sends request to Asink server
+	 */
 	public function start()
 	{
 		$this->prepareRequest();
